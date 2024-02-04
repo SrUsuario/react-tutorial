19f98bf9849e81c233d9fbbd28d3fde46f9eb3b6
SrUsuario <raul.parada040103@gmail.com>
2024-02-04 22:42:16 +0100

    Más mejoras en el proyecto 00-hola-mundo

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
-    const formatUserName = (userName) => `@${userName}`
 
     return (
         <section className='App'>
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="midudev" name="Miguel Ángel Durán" />
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing={false} userName="pheralb" name="Pablo Hernandez" />
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="elonmusk" name="Elon Musk" />
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="vxnder" name="Vanderhart" />
-        </section>
+            <TwitterFollowCard isFollowing userName="midudev">
+                Miguel Ángel Durán
+            </TwitterFollowCard>
+            <TwitterFollowCard isFollowing={false} userName="pheralb">
+                Pablo Hernandez
+            </TwitterFollowCard>
+        </section >
     )
 }
